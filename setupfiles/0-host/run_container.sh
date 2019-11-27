53ea71cc7dd451e701145c1692e71856d4b3e65a
Harunobu Kurokawa <harunobu.kurokawa.dn@renesas.com>
2019-11-27 13:39:22 +0900

    Add support IC communication feature
    Use remoting plugin streaming
    
    Signed-off-by: Harunobu Kurokawa <harunobu.kurokawa.dn@renesas.com>

--- setupfiles/0-host/run_container.sh
+++ setupfiles/0-host/run_container.sh
@@ -7,6 +7,7 @@ export WAYLAND_DISPLAY=wayland-1
 /usr/bin/lxc-start -n cluster
 /usr/bin/layer-add-surfaces -s 1 -l 2 -d HDMI-A-2
 
+sleep 1 
 
 /bin/rm /lxc/guest/home/0/c13\:*
 /bin/rm /lxc/guest/home/0/b8\:*
@@ -16,10 +17,11 @@ export WAYLAND_DISPLAY=wayland-1
 /bin/cp /run/udev/data/b8\:* /lxc/guest/home/0
 
 /usr/bin/lxc-start -n guest 
-/usr/bin/lxc-device -n guest add /dev/sda1
 
 /bin/sleep 4
-/usr/bin/layer-add-surfaces -s 1 -l 1 -d HDMI-A-1
+/home/0/start-ivi.sh
+#/usr/bin/layer-add-surfaces -s 1 -l 1 -d HDMI-A-1
+/usr/bin/lxc-device -n guest add /dev/sda1
 
 #/usr/bin/lxc-attach -n guest
 #/bin/mount /dev/sda1 /media
--- setupfiles/0-host/start-ivi.sh
+++ setupfiles/0-host/start-ivi.sh
@@ -2,5 +2,8 @@ export XDG_RUNTIME_DIR=/run/platform/display
 export WAYLAND_DISPLAY=wayland-1
 
 
-/usr/bin/layer-add-surfaces -s 1 -l 1 -d HDMI-A-1
+/usr/bin/layer-add-surfaces -s 2 -l 1 -d HDMI-A-1
+
+/usr/bin/layer-add-surfaces -s 1 -l 3 -d remote-1
+
 
